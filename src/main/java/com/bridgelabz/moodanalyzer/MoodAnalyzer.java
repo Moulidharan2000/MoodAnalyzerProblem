b5fd283e95c5c08acf59fb34fb5f0ff59a0a3faa
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-02-10 20:09:14 +0530

    Added : Analyse Null Mood should throw MoodAnalysisExcepion

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,28 +1,23 @@
 package com.bridgelabz.moodanalyzer;
 
-import java.util.Scanner;
-
 public class MoodAnalyzer {
 	
-	public void analyzeMood(String mood)throws MoodAnalysisException{
+	public String analyzeMood(String mood)throws MoodAnalysisException{
 		
-		if(mood == null || mood.isEmpty())
-			throw new MoodAnalysisException("Mood Cannot be null or empty", MoodAnalysisError.EMPTY_MOOD);
-		else if(mood.contains("Happy"))
-			System.out.println("HAPPY!!!");
-		else
-			System.out.println("SAD");
+		try {
+			if(mood == null || mood.isEmpty())
+				throw new MoodAnalysisException("Mood Cannot be null or empty", MoodAnalysisError.EMPTY_MOOD);
+			else if(mood.contains("Happy"))
+				return "HAPPY!!!";
+		}catch(MoodAnalysisException e) {
+			return "Invalid Mood";
+		}
+		return "SAD";
 	}
 	
 	public static void main(String[] args) throws MoodAnalysisException{
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the Message : ");
-		try{
 			MoodAnalyzer analyze = new MoodAnalyzer();
-			analyze.analyzeMood(scan.next());
-		}catch(MoodAnalysisException e) {
-			System.out.println("Invalid Mood");
-		}
+			analyze.analyzeMood(null);
 	}
 }
